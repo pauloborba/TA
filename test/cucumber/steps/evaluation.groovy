5570cdaa841bf56c0f8b35ede42a69b15fd7dfeb
unknown <mcbsf@g5c13.windows.cin.ufpe.br>
2015-04-16 16:56:54 -0300

    terminando 2o cenario

--- test/cucumber/steps/evaluation.groovy
+++ test/cucumber/steps/evaluation.groovy
@@ -17,6 +17,7 @@ Given (~'^I am on Teaching Assistant Teacher's home page$') {
 	def evaluation_E2 = UEvaluation.createEvaluation()
 
 }
+
 //	When I select for "evaluations"
 When (~'^I select for "([^"]*)"$') {
 	String button ->	
@@ -25,19 +26,33 @@ When (~'^I select for "([^"]*)"$') {
 }
 
 //  Then I should see the Visualization Page
-Then (~'^I should see the "([^"]*)"$'){
-	->
-	redirect(view: "VisualisationPage")
+Then (~'^I should see the Visualization Page$'){
+	at VisualizationPage
 }
-//	When I search for "evaluation" on the "search" field
-When (~'^I search for "([^"]*)" on the "([^"]*)" field$'){
-	String title, searchField ->
-	at VisualisationPage
+//	When I search for "evaluation" on the Search field
+When (~'^I search for "([^"]*)" on the Search field$'){
+	String title ->
+	at VisualizationPage
 	def evaluationList = PicaProsCus.findByTitle(title)
 
 }
 //	Then I should see all the evaluations that have the name writted
 Then(~'^I should see the all the evaluations that have the name writted$'){
-	
+	at VisualizationPage
+	$('li').find(evaluationList)
 }
+
+/*
+Scenario: select an evaluation to see it
+  	Given I am on Teaching Assistant Teacher's home page
+  	And add the evaluation "evaluation E1"
+  	And add the evaluation "evaluation E2"
+  	When I search for "evaluation"
+  	ps: passo já implementado até aqui
+	And I Choose "evaluation E1"
+	*/
+When()
+	/*
+	Then "evaluation E1" is open
+*/
 
