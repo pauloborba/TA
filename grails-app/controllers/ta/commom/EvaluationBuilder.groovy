e310cedf9e2dc60dc4f68fac2ac7ff777b43ad62
Leo Schettini <ljsa@cin.ufpe.br>
2015-04-18 14:44:03 -0300

    Builder de avaliacao

---
+++ grails-app/controllers/ta/commom/EvaluationBuilder.groovy
@@ -0,0 +1,31 @@
+package ta.commom
+
+class EvaluationBuilder implements IEvaluationBuilder {
+
+	private Evaluation _Evaluation
+
+	public void createEvaluation() {
+		_Evaluation = new Evaluation()
+	}
+
+	public void setEvaluationTitle(String title) {
+		_Evaluation.title = title
+	}
+
+	public int addEvaluationQuestion(String question) {
+		_Evaluation.questions.add(questioin)
+
+	}
+
+	public void setQuestionAnswer(int questionIndex, String answer) {
+		_Evaluation.questions[questionIndex].answer = answer
+	}
+
+	public void addQuestionAlternative(int questionIndex, String alternative){
+		_Evaluation.questions[questionIndex].alternatives.add(alternative)
+	}
+
+	public Evaluation getEvaluation() {
+		return _Evaluation
+	}
+}
