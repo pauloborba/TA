57b9d01849290828f1ed25056477b550f1a905e4
Leo Schettini <ljsa@cin.ufpe.br>
2015-05-02 21:16:53 -0300

    ajustes

--- grails-app/domain/taL/Evaluation.groovy
+++ grails-app/domain/taL/Evaluation.groovy
@@ -4,6 +4,10 @@ class Evaluation {
 	public String title
 	public List<Question> questions
 
+	static constraints = {
+        title blank: false
+    }
+
 	public String getTitle() { title }
 
 	public Question getQuestion(int index) { questions[index] }
