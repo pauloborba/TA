e7c30cf7818d26652ea282af92880ade0da9e20c
Leo Schettini <ljsa@cin.ufpe.br>
2015-05-02 21:00:00 -0300

    implementacao de feature RegisterEvaluation

---
+++ grails-app/domain/ta-leo/Evaluation.groovy
@@ -0,0 +1,10 @@
+package taL
+
+class Evaluation {
+	public String title
+	public List<Question> questions
+
+	public String getTitle() { title }
+
+	public Question getQuestion(int index) { questions[index] }
+}
