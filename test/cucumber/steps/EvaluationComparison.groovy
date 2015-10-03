05f9e699d1f7fea0f940cab310e57457b1646a62
ehammo <ehammo@cin.ufpe.br>
2015-10-03 15:52:11 -0300

    AutoEvaluation tests - First changes

--- test/cucumber/steps/EvaluationComparison.groovy
+++ test/cucumber/steps/EvaluationComparison.groovy
@@ -1,35 +1,55 @@
-Feature: Evaluation -> Auto-evaluation comparison
-as a professor
-I want to compare my evaluation of the student with the evaluation made by him
-so that I can call him if there is any discrepancies.
+package steps
+
+import pages.EvaluationComparisonPage
+import ta.Evaluation
+import ta.EvaluationController
+
+this.metaClass.mixin(cucumber.api.groovy.Hooks)
+this.metaClass.mixin(cucumber.api.groovy.EN)
 
 
-GUI
 
-Success
-
+/*
 Given the student "X" appear in the list of student that sent their auto-Evaluation
 When I select the compare grades option
 And choose to compare student "X" grades
 Then I can see a detailed table with both student and the professor Evaluations being put, in each criterion, side by side in the screen.
- 
-Failure
+*/
 
+Given (~'^Given the student "([^"]*)" appear in the list of student that sent their auto-Evaluation$'){
+    String text ->
+
+
+
+}
+
+When (~'^I select the compare grades option$') { ->
+
+        at EvaluationComparisonPage
+        page.click()
+}
+
+And (~'^choose to compare student "([^"]*)" grades$'){
+    String text->
+
+
+}
+
+/*
 Given the student "X" don't appear in the list of student that sent their auto-Evaluation
 When I select the compare grades option
 And choose to compare student "X" grades
 Then I can see a error message with a go-back button to go to the main page.
+*/
 
-Controller
-
-Success
-
+/*
 Given Student "X"’s Auto-Evaluation is on the database
 When the system requires the  Evaluation -> Auto-evaluation comparison
 Then the system returns a detailed table with both student and the professor grades.
+*/
 
-Failure
-
+/*
 Given Student "X"’s Auto-Evaluation isn’t on the database
 When the system requires the  Evaluation -> Auto-evaluation comparison
 Then the system returns an exception.
+*/
