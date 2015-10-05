e729b6a1aa41d664e84a215eabbed23d7d69e663
ehammo <ehammo@cin.ufpe.br>
2015-10-05 07:37:26 -0300

    Update EvaluationComparison.groovy

--- test/cucumber/steps/EvaluationComparison.groovy
+++ test/cucumber/steps/EvaluationComparison.groovy
@@ -17,7 +17,7 @@ Then I can see a detailed table with both student and the professor Evaluations 
 
 Given (~'^Given the student "([^"]*)" appear in the list of student that sent their auto-Evaluation$'){
     String text ->
-        assert Student.findByName(text).AutoEvaluations!=null:"test failed"
+        assert Student.findByName(text).AutoEvaluations!=null
 
 }
 
@@ -47,7 +47,7 @@ Then I can see a error message with a go-back button to go to the main page.
 
 Given (~'^Given the student "([^"]*)" don`t appear in the list of student that sent their auto-Evaluation$'){
     String text ->
-        assert Student.findByName(text).AutoEvaluations==null:"test failed"
+        assert Student.findByName(text).AutoEvaluations==null
 
 }
 
@@ -76,7 +76,7 @@ Then the system returns a detailed table with both student and the professor gra
 
 Given (~'^Given the student "([^"]*)"`s Auto-Evaluation is on the database$'){
     String text ->
-        assert Student.findByName(text).AutoEvaluations!=null:"test failed"
+        assert Student.findByName(text).AutoEvaluations!=null
 
 }
 
@@ -99,7 +99,7 @@ Then the system returns an exception.
 
 Given (~'^Given the student "([^"]*)"`s Auto-Evaluation isn`t on the database$'){
     String text ->
-        assert Student.findByName(text).AutoEvaluations==null:"test failed"
+        assert Student.findByName(text).AutoEvaluations==null
 
 }
 
@@ -111,5 +111,6 @@ When (~'^the system requires the  Evaluation -> Auto-evaluation comparison$') {
 
 Then (~'^the system returns an exception$'){->
     at ShowComparisionPage
-    assert page.AutoEvaContent==null:"Auto Evaluation null"
+    assert page.AutoEvaContent==null
+    page.showError();
 }
