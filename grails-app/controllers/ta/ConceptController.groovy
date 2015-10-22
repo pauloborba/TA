b015ee2bd7e3ed0e7fa3099524c53ebdf8505f02
psg2 <psg2@cin.ufpe.br>
2015-10-22 20:29:02 -0300

    Correção de Conflitos numero 2

--- grails-app/controllers/ta/ConceptController.groovy
+++ grails-app/controllers/ta/ConceptController.groovy
@@ -1,21 +1,10 @@
 package ta
 
 import commom.SheetBuilder
+
 import static org.springframework.http.HttpStatus.*
 import grails.transaction.Transactional
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-import static org.springframework.http.HttpStatus.*
-import grails.transaction.Transactional
-
->>>>>>> 6e87b84962abbf18b068e9aef5aa0b8ae9cb1c47
-=======
-import static org.springframework.http.HttpStatus.*
-import grails.transaction.Transactional
-
->>>>>>> 6e87b84962abbf18b068e9aef5aa0b8ae9cb1c47
 @Transactional(readOnly = true)
 class ConceptController {
     SheetBuilder builder = new SheetBuilder()
@@ -25,46 +14,19 @@ class ConceptController {
 
     def index(Integer max) {
         params.max = Math.min(max ?: 10, 100)
-<<<<<<< HEAD
-<<<<<<< HEAD
-        respond Concept.list(params), model: [conceptInstanceCount: Student.Concept.count()]
-    }
-
-    def show(Student.Concept conceptInstance) {
-=======
-=======
->>>>>>> 6e87b84962abbf18b068e9aef5aa0b8ae9cb1c47
         respond Concept.list(params), model: [conceptInstanceCount: Concept.count()]
     }
 
     def show(Concept conceptInstance) {
-<<<<<<< HEAD
->>>>>>> 6e87b84962abbf18b068e9aef5aa0b8ae9cb1c47
-=======
->>>>>>> 6e87b84962abbf18b068e9aef5aa0b8ae9cb1c47
         respond conceptInstance
     }
 
     def create() {
-<<<<<<< HEAD
-<<<<<<< HEAD
-        respond new Student.Concept(params)
-    }
-
-    @Transactional
-    def save(Student.Concept conceptInstance) {
-=======
-=======
->>>>>>> 6e87b84962abbf18b068e9aef5aa0b8ae9cb1c47
         respond new Concept(params)
     }
 
     @Transactional
     def save(Concept conceptInstance) {
-<<<<<<< HEAD
->>>>>>> 6e87b84962abbf18b068e9aef5aa0b8ae9cb1c47
-=======
->>>>>>> 6e87b84962abbf18b068e9aef5aa0b8ae9cb1c47
         if (conceptInstance == null) {
             notFound()
             return
@@ -86,90 +48,17 @@ class ConceptController {
         }
     }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-    def edit(Student.Concept conceptInstance) {
-=======
     def edit(Concept conceptInstance) {
->>>>>>> 6e87b84962abbf18b068e9aef5aa0b8ae9cb1c47
-=======
-    def edit(Concept conceptInstance) {
->>>>>>> 6e87b84962abbf18b068e9aef5aa0b8ae9cb1c47
         respond conceptInstance
     }
 
     @Transactional
-<<<<<<< HEAD
-<<<<<<< HEAD
-    def update(Student.Concept conceptInstance) {
-=======
     def update(Concept conceptInstance) {
->>>>>>> 6e87b84962abbf18b068e9aef5aa0b8ae9cb1c47
-        if (conceptInstance == null) {
-            notFound()
-            return
-        }
-<<<<<<< HEAD
-
-        if (conceptInstance.hasErrors()) {
-            respond conceptInstance.errors, view: 'edit'
-            return
-        }
-
-        conceptInstance.save flush: true
-
-        request.withFormat {
-            form multipartForm {
-                flash.message = message(code: 'default.updated.message', args: [message(code: 'Concept.label', default: 'Concept'), conceptInstance.id])
-                redirect conceptInstance
-            }
-            '*' { respond conceptInstance, [status: OK] }
-        }
-    }
-
-    @Transactional
-    def delete(Student.Concept conceptInstance) {
-
-=======
-    def update(Concept conceptInstance) {
->>>>>>> 6e87b84962abbf18b068e9aef5aa0b8ae9cb1c47
         if (conceptInstance == null) {
             notFound()
             return
         }
 
-<<<<<<< HEAD
-        conceptInstance.delete flush: true
-
-        request.withFormat {
-            form multipartForm {
-                flash.message = message(code: 'default.deleted.message', args: [message(code: 'Concept.label', default: 'Concept'), conceptInstance.id])
-                redirect action: "index", method: "GET"
-            }
-            '*' { render status: NO_CONTENT }
-        }
-    }
-
-    protected void notFound() {
-        request.withFormat {
-            form multipartForm {
-                flash.message = message(code: 'default.not.found.message', args: [message(code: 'concept.label', default: 'Concept'), params.id])
-                redirect action: "index", method: "GET"
-            }
-            '*' { render status: NOT_FOUND }
-        }
-    }
-
-    def upload(){
-
-    }
-
-    def importSheet(Sheet sheet){
-        this.sheet = sheet;
-    }
-
-=======
-
         if (conceptInstance.hasErrors()) {
             respond conceptInstance.errors, view: 'edit'
             return
@@ -186,24 +75,6 @@ class ConceptController {
         }
     }
 
-=======
-        if (conceptInstance.hasErrors()) {
-            respond conceptInstance.errors, view: 'edit'
-            return
-        }
-
-        conceptInstance.save flush: true
-
-        request.withFormat {
-            form multipartForm {
-                flash.message = message(code: 'default.updated.message', args: [message(code: 'Concept.label', default: 'Concept'), conceptInstance.id])
-                redirect conceptInstance
-            }
-            '*' { respond conceptInstance, [status: OK] }
-        }
-    }
-
->>>>>>> 6e87b84962abbf18b068e9aef5aa0b8ae9cb1c47
     @Transactional
     def delete(Concept conceptInstance) {
 
@@ -256,10 +127,6 @@ class ConceptController {
 //        flash.message = "Invalid file format!"
     }
 
-<<<<<<< HEAD
->>>>>>> 6e87b84962abbf18b068e9aef5aa0b8ae9cb1c47
-=======
->>>>>>> 6e87b84962abbf18b068e9aef5aa0b8ae9cb1c47
     def reset(){
         builder.removeSheet();
         this.sheet = null;
@@ -270,8 +137,4 @@ class ConceptController {
 
 //def save(){
 //    return this.sheet.save();
-<<<<<<< HEAD
 //}
-=======
-//}
->>>>>>> 6e87b84962abbf18b068e9aef5aa0b8ae9cb1c47
--- grails-app/views/concept/upload.gsp
+++ grails-app/views/concept/upload.gsp
@@ -2,42 +2,31 @@
 <%@ page import="ta.Concept" %>
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta name="layout" content="main">
-		<g:set var="entityName" value="${message(code: 'concept.label', default: 'Concept')}" />
-		<title>Upload Sheet</title>
-	</head>
-	<body>
-		<a href="#list-concept" class="skip" tabindex="-1"><g:message code="default.link.skip.label" default="Skip to content&hellip;"/></a>
-		<div class="nav" role="navigation">
-			<ul>
-				<li><a class="home" href="${createLink(uri: '/')}"><g:message code="default.home.label"/></a></li>
-				<li><g:link class="create" action="create"><g:message code="default.new.label" args="[entityName]" /></g:link></li>
-			</ul>
-		</div>
-		<div>
-			<h1>Upload Sheet</h1>
-			<g:if test="${flash.message}">
-				<div class="message" role="status">${flash.message}</div>
-			</g:if>
+<head>
+	<meta name="layout" content="main">
+	<g:set var="entityName" value="${message(code: 'concept.label', default: 'Concept')}" />
+	<title>Upload Sheet</title>
+</head>
+<body>
+<a href="#list-concept" class="skip" tabindex="-1"><g:message code="default.link.skip.label" default="Skip to content&hellip;"/></a>
+<div class="nav" role="navigation">
+	<ul>
+		<li><a class="home" href="${createLink(uri: '/')}"><g:message code="default.home.label"/></a></li>
+		<li><g:link class="create" action="create"><g:message code="default.new.label" args="[entityName]" /></g:link></li>
+	</ul>
+</div>
+<div>
+	<h1>Upload Sheet</h1>
+	<g:if test="${flash.message}">
+		<div class="message" role="status">${flash.message}</div>
+	</g:if>
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-			<input type="file" name = "datafile" size = "40"/>
-			<input type="submit" value="Upload" name ="submit"/>
-=======
-=======
->>>>>>> 6e87b84962abbf18b068e9aef5aa0b8ae9cb1c47
-			<g:uploadForm controller="concept" action = "submit">
+	<g:uploadForm controller="concept" action = "submit">
 
-				<input type="file" name = "datafile" size = "40"/>
-				%{--<g:actionSubmit  name="submit" value="submit"/>--}%
-				<g:submitButton  name="submit" value="submit"/>
-			</g:uploadForm>
-<<<<<<< HEAD
->>>>>>> 6e87b84962abbf18b068e9aef5aa0b8ae9cb1c47
-=======
->>>>>>> 6e87b84962abbf18b068e9aef5aa0b8ae9cb1c47
-		</div>
-	</body>
+		<input type="file" name = "datafile" size = "40"/>
+	%{--<g:actionSubmit  name="submit" value="submit"/>--}%
+		<g:submitButton  name="submit" value="submit"/>
+	</g:uploadForm>
+</div>
+</body>
 </html>
