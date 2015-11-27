e717eaa75e5699afdda133247c660fbfe966c60d
gaabs <gaabs@cin.ufpe.br>
2015-11-27 10:54:12 -0300

    gebconfig windows

--- test/functional/GebConfig.groovy
+++ test/functional/GebConfig.groovy
@@ -1,35 +1,35 @@
-//import org.openqa.selenium.chrome.ChromeDriver
-//
-//driver = {
-//    File file = new File("/home/ess/TA2/chromedrivers/chromedriverlinux64");
-//    System.setProperty("webdriver.chrome.driver", file.getAbsolutePath()  );
-//    new ChromeDriver();
-//}
-//
-//baseUrl = "http://localhost:8070/"
-
 import org.openqa.selenium.chrome.ChromeDriver
-import org.openqa.selenium.firefox.FirefoxDriver
-import org.openqa.selenium.firefox.FirefoxProfile
 
 driver = {
-//def driver = new HtmlUnitDriver()
-//driver.javascriptEnabled = true
-//driver
-    def driver = new FirefoxDriver()
-    driver
+    File file = new File("C:/Users/GioIdeaProjects/TA2/chromedrivers/chromedriver.exe");
+    System.setProperty("webdriver.chrome.driver", file.getAbsolutePath()  );
+    new ChromeDriver();
 }
 
-environments {
-    // run as “grails -Dgeb.env=chrome test-app”
-    // See: http://code.google.com/p/selenium/wiki/ChromeDriver
-    chrome {
-        driver = { new ChromeDriver() }
-    }
+baseUrl = "http://localhost:8070/"
 
-    // run as “grails -Dgeb.env=firefox test-app”
-    // See: http://code.google.com/p/selenium/wiki/FirefoxDriver
-    firefox {
-        driver = { new FirefoxDriver() }
-    }
-}
+//import org.openqa.selenium.chrome.ChromeDriver
+//import org.openqa.selenium.firefox.FirefoxDriver
+//import org.openqa.selenium.firefox.FirefoxProfile
+//
+//driver = {
+////def driver = new HtmlUnitDriver()
+////driver.javascriptEnabled = true
+////driver
+//    def driver = new FirefoxDriver()
+//    driver
+//}
+//
+//environments {
+//    // run as “grails -Dgeb.env=chrome test-app”
+//    // See: http://code.google.com/p/selenium/wiki/ChromeDriver
+//    chrome {
+//        driver = { new ChromeDriver() }
+//    }
+//
+//    // run as “grails -Dgeb.env=firefox test-app”
+//    // See: http://code.google.com/p/selenium/wiki/FirefoxDriver
+//    firefox {
+//        driver = { new FirefoxDriver() }
+//    }
+//}
