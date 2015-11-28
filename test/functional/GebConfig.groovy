f8b61e39289472be107f469d5723c903b319ad3e
imvm <imvm@cin.ufpe.br>
2015-11-28 11:51:04 -0300

    travis fix

--- test/functional/GebConfig.groovy
+++ test/functional/GebConfig.groovy
@@ -1,14 +1,4 @@
-﻿//import org.openqa.selenium.chrome.ChromeDriver
-//
-//driver = {
-//    File file = new File("/home/ess/TA5/chromedrivers/chromedriverlinux64");
-//    System.setProperty("webdriver.chrome.driver", file.getAbsolutePath()  );
-//    new ChromeDriver();
-//}
-//
-//baseUrl = "http://localhost:8070/"
-
-import org.openqa.selenium.chrome.ChromeDriver
+﻿import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.firefox.FirefoxDriver
 import org.openqa.selenium.firefox.FirefoxProfile
 
