e359124ca214b52102c11ca4ea406ee4146097ce
imvm <imvm@cin.ufpe.br>
2015-11-28 20:16:57 -0200

    Fixing Travis invalid variable bug

--- test/functional/GebConfig.groovy
+++ test/functional/GebConfig.groovy
@@ -1,14 +1,15 @@
-﻿//import org.openqa.selenium.chrome.ChromeDriver
-//
-//driver = {
-////  File file = new File("C:/Users/Pedro/IdeaProjects/TA/chromedrivers/chromedriver.exe");
-//    File file = new File("/home/ess/TA2/chromedrivers/chromedriverlinux64");
-//    System.setProperty("webdriver.chrome.driver", file.getAbsolutePath()  );
-//    new ChromeDriver();
-//}
-//
-//baseUrl = "http://localhost:8070/"
+﻿/*
+import org.openqa.selenium.chrome.ChromeDriver
 
+driver = {
+//    File file = new File("/home/ess/TA2/chromedrivers/chromedriver.exe");
+    File file = new File("/home/ess/TA2/chromedrivers/chromedriverlinux64");
+    System.setProperty("webdriver.chrome.driver", file.getAbsolutePath()  );
+    new ChromeDriver();
+}
+
+baseUrl = "http://localhost:8070/"
+/*
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.firefox.FirefoxDriver
 import org.openqa.selenium.firefox.FirefoxProfile
