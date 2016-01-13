a7ef8e9eefd518f12f286887bb711b84ccd00f0f
Eduardo Ximenes <ehxmm@cin.ufpe.br>
2016-01-13 00:37:17 -0300

    Update RegisterTeacherPage.groovy

--- test/functional/pages/RegisterTeacherPage.groovy
+++ test/functional/pages/RegisterTeacherPage.groovy
@@ -1,25 +1,25 @@
 package pages
-
+ 
 import geb.Page
-
+ 
 class RegisterTeacherPage extends Page {
-
+ 
     static url = "/TA/teachers/create"
-
+ 
     static at = {
-
-        title ==~ /Create Teachers/
-
+        String model = "Teachers"
+        String msg = GetPageTitle.getMessage("default.create.label", "Teachers")
+        title ==~ msg
     }
-
+ 
     def fillCpf(String name, String cpf){
-
+ 
         $("form").name = name
         $("form").cpf = cpf
-
+ 
     }
-
+ 
     def showError(){}
     def showConfirmation(){}
-
+ 
 }
