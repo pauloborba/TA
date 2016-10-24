f3c2d5fe7fe7683d55dc249b8c24980b31d09c4d
vapp <v.augusto.1994@gmail.com>
2016-10-24 11:00:23 -0200

    Adding steps' implementation

--- test/cucumber/steps/RelatorioAlunosSteps.groovy
+++ test/cucumber/steps/RelatorioAlunosSteps.groovy
@@ -20,141 +20,186 @@ this.metaClass.mixin(cucumber.api.groovy.EN)
 
 
 
-
-
-Given(~/^o item "([^"]*)" do aluno "([^"]*)" está vazio$/) { String arg1, String arg2 ->
-
+Given(~/^a Média Final do aluno "([^"]*)" é igual a (\d+)$/) { String arg1, int arg2 ->
+    to menuPrincipalPage
+    at menuPrincipalPage
+    page.criarAluno(arg1, 0, 3, 7, arg2)       // cria-se o aluno com nome, manas avaliadas, mpas avaliadas, mas avaliadas e nota !! Tambem vai ter um construtor para a nota da final
+}
+And(~/^a Média Final da aluna "([^"]*)" é igual a (\d+)$/) { String arg1, int arg2 ->
+    at menuPrincipalPage
+    page.criarAluno(arg1, 0, 5, 5, arg2, arg2)   // aqui eh o construtor com a nota da final
+}
+And(~/^a Média Final do aluno "([^"]*)" é (\d+)$/) { String arg1, int arg2 ->
+    at menuPrincipalPage
+    page.criarAluno(arg1, 0, 5, 5, arg2, arg2)   // aqui eh o construtor com a nota da final
+}
+And(~/^só há (\d+) alunos na turma$/) { int arg1 ->
+    at menuPrincipalPage
+    page.quantidadeTotalDeAlunos(arg1)
+}
+And(~/^há um total de (\d+) critérios avaliados$/) { int arg1 ->
+    at menuPrincipalPage
+    page.quantidadeCriteriosAvaliados(arg1)
 }
 When(~/^tento gerar o Relatório de notas no menu principal$/) { ->
-
-}
-Then(~/^é mostrada uma mensagem indicando que ainda não foram cadastradas todas as notas no menu principal$/) { ->
-
-}
-
-
-
-Given(~/^a "([^"]*)" do aluno "([^"]*)" é igual a "([^"]*)"$/) { String arg1, String arg2, String arg3 ->
-
-}
-And(~/^a "([^"]*)" da aluna "([^"]*)" é igual a "([^"]*)"$/) { String arg1, String arg2, String arg3 ->
-
-}
-And(~/^a "([^"]*)" do aluno "([^"]*)" é "([^"]*)"$/) { String arg1, String arg2, String arg3 ->
-
-}
-When(~/^tento gerar o Relatório de notas no menu principal$/) { ->
-
+    at menuPrincipalPage
+    page.gerarRelatorio()
 }
 Then(~/^a linha do aluno "([^"]*)" será da cor "([^"]*)" no Relatório de notas$/) { String arg1, String arg2 ->
-
+    to relatorioAlunosPage
+    at relatorioAlunosPage
+    assert page.isColor(arg2, arg1)
 }
 And(~/^a coluna "([^"]*)" do aluno "([^"]*)" será vazia$/) { String arg1, String arg2 ->
-
+    at relatorioAlunosPage
+    assert page.isNull(arg1, arg1)
 }
 And(~/^a linha da aluna "([^"]*)" será da cor "([^"]*)" no Relatório de notas$/) { String arg1, String arg2 ->
-
+    at relatorioAlunosPage
+    assert page.isColor(arg2, arg1)
 }
 And(~/^a linha do aluno "([^"]*)" será da cor "([^"]*)" no Relatório de notas$/) { String arg1, String arg2 ->
-
+    at relatorioAlunosPage
+    assert page.isColor(arg2, arg1)
 }
 
 
 
-
-Given(~/^o aluno "([^"]*)" teve "([^"]*)" "([^"]*)"$/) { String arg1, String arg2, String arg3 ->
-
-}
-And(~/^o aluno "([^"]*)" teve "([^"]*)" "([^"]*)"$/) { String arg1, String arg2, String arg3 ->
-
-}
-And(~/^o aluno "([^"]*)" teve "([^"]*)" "([^"]*)"$/) { String arg1, String arg2, String arg3 ->
-
-}
-When(~/^tento gerar o Relatório de notas no menu principal$/) { ->
-
-}
-Then(~/^a coluna "([^"]*)" do aluno "([^"]*)" será "([^"]*)"$/) { String arg1, String arg2, String arg3 ->
-
-}
-
-
-
-
-Given(~/^a aluna "([^"]*)" teve "([^"]*)" igual a "([^"]*)"$/) { String arg1, String arg2, String arg3 ->
-
-}
-And(~/^a aluna "([^"]*)" teve "([^"]*)" igual a "([^"]*)"$/) { String arg1, String arg2, String arg3 ->
-
-}
-When(~/^tento gerar o Relatório de notas no menu principal$/) { ->
-
-}
-Then(~/^a aluna "([^"]*)" terá a a coluna "([^"]*)" igual a "([^"]*)"$/) { String arg1, String arg2, String arg3 ->
-
-}
-
-
-
-
-Given(~/^o aluno "([^"]*)" tem "([^"]*)" igual a "([^"]*)"$/) { String arg1, String arg2, String arg3 ->
-
-}
-And(~/^o aluno "([^"]*)" tem "([^"]*)" igual a "([^"]*)"$/) { String arg1, String arg2, String arg3 ->
-
-}
-And(~/^o aluno "([^"]*)" tem "([^"]*)" igual a "([^"]*)"$/) { String arg1, String arg2, String arg3 ->
-
-}
-And(~/^o aluno "([^"]*)" tem "([^"]*)" igual a "([^"]*)"$/) { String arg1, String arg2, String arg3 ->
-
-}
-And(~/^a aluna "([^"]*)" tem "([^"]*)" igual a "([^"]*)"$/) { String arg1, String arg2, String arg3 ->
-
+Given(~/^o aluno "([^"]*)" teve (\d+) MANAs Avaliadas, (\d+) MPAs Avaliadas e (\d+) MAs Avaliadas$/) { String arg1, int arg2, int arg3, int arg4 ->
+    to menuPrincipalPage
+    at menuPrincipalPage
+    page.criarAluno(arg1, arg2, arg3, arg4, 9)
 }
 And(~/^só há (\d+) alunos na turma$/) { int arg1 ->
-
+    at menuPrincipalPage
+    page.quantidadeTotalDeAlunos(arg1)
+}
+And(~/^há um total de (\d+) critérios avaliados$/) { int arg1 ->
+    at menuPrincipalPage
+    page.quantidadeCriteriosAvaliados(arg1)
 }
 When(~/^tento gerar o Relatório de notas no menu principal$/) { ->
-
+    at menuPrincipalPage
+    page.gerarRelatorio()
 }
-Then(~/^o "([^"]*)" será igual a "([^"]*)"$/) { String arg1, String arg2 ->
-
-}
-And(~/^o "([^"]*)" será igual a "([^"]*)"$/) { String arg1, String arg2 ->
-
-}
-And(~/^o "([^"]*)" será igual a "([^"]*)"$/) { String arg1, String arg2 ->
-
-}
-And(~/^o "([^"]*)" será igual a "([^"]*)"$/) { String arg1, String arg2 ->
-
-}
-And(~/^o "([^"]*)" será igual a "([^"]*)"$/) { String arg1, String arg2 ->
-
+Then(~/^a coluna "([^"]*)" do aluno "([^"]*)" será (\d+)$/) { String arg1, String arg2, String arg3 ->
+    to relatorioAlunosPage
+    at relatorioAlunosPage
+    assert isColumn(arg1, arg2, arg3)
 }
 
 
 
-
-Given(~/^o número total de critérios cadastrados para a turma é "([^"]*)"$/) { String arg1 ->
-
-}
-And(~/^a aluna "([^"]*)" tem "([^"]*)" igual a "([^"]*)"$/) { String arg1, String arg2, String arg3 ->
-
-}
-And(~/^a aluna "([^"]*)" tem "([^"]*)" igual a "([^"]*)"$/) { String arg1, String arg2, String arg3 ->
-
-}
-And(~/^o aluno "([^"]*)" tem "([^"]*)" igual a "([^"]*)"$/) { String arg1, String arg2, String arg3 ->
-
+Given(~/^a aluna "([^"]*)" teve Nota igual a (\d+) e teve Final igual a (\d+)$/) { String arg1, int arg2, int arg3 ->
+    to menuPrincipalPage
+    at menuPrincipalPage
+    page.criarAluno(arg1, 0, 5, 5, arg2, arg3)
 }
 And(~/^só há (\d+) alunos na turma$/) { int arg1 ->
-
+    at menuPrincipalPage
+    page.quantidadeTotalDeAlunos(arg1)
+}
+And(~/^há um total de (\d+) critérios avaliados$/) { int arg1 ->
+    at menuPrincipalPage
+    page.quantidadeCriteriosAvaliados(arg1)
 }
 When(~/^tento gerar o Relatório de notas no menu principal$/) { ->
-
+    at menuPrincipalPage
+    page.gerarRelatorio()
 }
-Then(~/^o "([^"]*)" será igual a "([^"]*)"$/) { String arg1, String arg2 ->
+Then(~/^a aluna "([^"]*)" terá a a coluna "([^"]*)" igual a (\d+)$/) { String arg1, String arg2, int arg3 ->
+    to relatorioAlunosPage
+    at relatorioAlunosPage
+    assert isColumn(arg2, arg1, arg3)
+}
 
+
+
+
+Given(~/^o aluno "([^"]*)" tem Média Final igual a (\d+)$/) { String arg1, int arg2 ->
+    to menuPrincipalPage
+    at menuPrincipalPage
+    page.criarAluno(arg1, 0, 3, 7, arg2)
+}
+And(~/^o aluno "([^"]*)" tem Média Final igual a (\d+)$/) { String arg1, int arg2 ->
+    at menuPrincipalPage
+    page.criarAluno(arg1, 0, 5, 5, arg2, arg2)
+}
+And(~/^o aluno "([^"]*)" tem Média Final igual a (\d+)$/) { String arg1, int arg2 ->
+    at menuPrincipalPage
+    page.criarAluno(arg1, 0, 5, 5, arg2, arg2)
+}
+And(~/^o aluno "([^"]*)" tem Média Final igual a (\d+)$/) { String arg1, int arg2 ->
+    at menuPrincipalPage
+    page.criarAluno(arg1, 0, 5, 5, arg2, arg2)
+}
+And(~/^a aluna "([^"]*)" tem Média Final igual a (\d+)$/) { String arg1, int arg2 ->
+    at menuPrincipalPage
+    page.criarAluno(arg1, 0, 0, 10, arg2)
+}
+And(~/^só há (\d+) alunos na turma$/) { int arg1 ->
+    at menuPrincipalPage
+    page.quantidadeTotalDeAlunos(arg1)
+}
+And(~/^há um total de (\d+) critérios avaliados$/) { int arg1 ->
+    at menuPrincipalPage
+    page.quantidadeCriteriosAvaliados(arg1)
+}
+When(~/^tento gerar o Relatório de notas no menu principal$/) { ->
+    at menuPrincipalPage
+    page.gerarRelatorio()
+}
+Then(~/^o "([^"]*)" será igual a (\d+)"$/) { String arg1, int arg2 ->
+    to relatorioAlunosPage
+    at relatorioAlunosPage
+    assert isGeneralColumn(arg1, arg2)
+}
+And(~/^o "([^"]*)" será igual a (\d+)$/) { String arg1, int arg2 ->
+    at relatorioAlunosPage
+    assert isGeneralColumn(arg1, arg2)
+}
+And(~/^o "([^"]*)" será igual a (\d+)$/) { String arg1, int arg2 ->
+    at relatorioAlunosPage
+    assert isGeneralColumn(arg1, arg2)
+}
+And(~/^o "([^"]*)" será igual a (\d+)$/) { String arg1, int arg2 ->
+    at relatorioAlunosPage
+    assert isGeneralColumn(arg1, arg2)
+}
+And(~/^o "([^"]*)" será igual a (\d+)$/) { String arg1, int arg2 ->
+    at relatorioAlunosPage
+    assert isGeneralColumn(arg1, arg2)
+}
+
+
+
+
+Given(~/^o número total de critérios cadastrados para a turma é (\d+)$/) { String arg1 ->
+    to menuPrincipalPage
+    at menuPrincipalPage
+    page.quantidadeCriteriosAvaliados(arg1)
+}
+And(~/^a aluna "([^"]*)" tem Total de critérios avaliados igual a (\d+)$/) { String arg1, int arg2 ->
+    at menuPrincipalPage
+    page.criarAluno(arg1, 0, 0, arg2, 6)
+}
+And(~/^a aluna "([^"]*)" tem Total de critérios avaliados igual a (\d+)$/) { String arg1, int arg2 ->
+    at menuPrincipalPage
+    page.criarAluno(arg1, 0, 0, arg2, 10)
+}
+And(~/^o aluno "([^"]*)" tem Total de critérios avaliados igual a (\d+)$/) { String arg1, int arg2 ->
+    at menuPrincipalPage
+    page.criarAluno(arg1, 0, 0, arg2, 5)
+}
+And(~/^só há (\d+) alunos na turma$/) { int arg1 ->
+    at menuPrincipalPage
+    page.quantidadeTotalDeAlunos(arg1)
+}
+When(~/^tento gerar o Relatório de notas no menu principal$/) { ->
+    at menuPrincipalPage
+    page.gerarRelatorio()
+}
+Then(~/^o "([^"]*)" será igual a (\d+)$/) { String arg1, int arg2 ->
+    at relatorioAlunosPage
+    assert isGeneralColumn(arg1, arg2)
 }
