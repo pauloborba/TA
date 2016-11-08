bd2ca279236fa39975f35191851247255e948e13
Lavinia Paganini <lfp2@cin.ufpe.br>
2016-11-07 22:37:12 -0300

    [#128919787] Implementação dos testes

--- test/cucumber/steps/SituationStudentsSteps.groovy
+++ test/cucumber/steps/SituationStudentsSteps.groovy
@@ -5,14 +5,20 @@
 
 import cucumber.api.groovy.EN
 import cucumber.api.groovy.Hooks
+import pages.AddStudentsPage
+import pages.CreateCriterionPage
 import steps.AddStudentsTestDataAndOperations
 import steps.CommonTestDataAndOperations
 import steps.CriterionTestDataAndOperations
 import steps.EvaluationDataAndOperations
 import steps.SituationStudentsTestDataAndOperation
 import ta.Evaluation
+import ta.EvaluationsByCriterion
+import ta.SituationStudentsController
 import ta.Student
 import ta.StudentController
+
+import static pages.AddStudentsPage.*
 
 
 this.metaClass.mixin(Hooks)
@@ -27,7 +33,8 @@ this.metaClass.mixin(EN)
       Then a média de “João Vasconcelos”, com login "jvsn", em “Requisitos de Sistemas” continua sendo “MA”
  */
 
-Student studentToCheck
+List<EvaluationsByCriterion> criteriosLista
+
 
 Given(~'^o aluno "([^"]*)", com login "([^"]*)", possui conceitos "([^"]*)", "([^"]*)" e "([^"]*)" em "([^"]*)"$') {
     String aluno, login, conceito1, conceito2, conceito3, materia ->
@@ -43,14 +50,38 @@ Given(~'^o aluno "([^"]*)", com login "([^"]*)", possui conceitos "([^"]*)", "([
 }
 
 And(~'o aluno "([^"]*)", com login "([^"]*)", possui media "([^"]*)" em "([^"]*)"'){
-    String aluno, String login, String media, String conceito ->
-
+    String aluno, String login, Double media, String conceito ->
+        Student estudante = Student.findByName(aluno)
+        estudante.calcMedia()
+        assert(estudante.findEvaluationByCriterion(conceito).criterionAverage.equals(media))
 }
 
-When(~'eu solicito a visualização da situação do aluno "([^"]*)" com login "([^"]*)"'){
+When(~'eu solicito a situação do aluno "([^"]*)" com login "([^"]*)"'){
     String aluno, String login ->
-
+    Student estudante = Student.findByName(aluno)
+        criteriosLista = estudante.getCriteriaAndEvaluations()
 }
 Then(~'a média de "([^"]*)", com login "([^"]*)", em "([^"]*)" continua sendo "([^"]*)"'){
-    String aluno, String login, String conceito, String media ->
+    String aluno, String login, String conceito, Double media ->
+        assert(Student.findByName(aluno).getCriteriaAndEvaluations().equals(criteriosLista))
+        assert(Student.findByName(aluno).findEvaluationByCriterion(conceito).criterionAverage.equals(media))
 }
+
+/*
+#GUI
+    Scenario: Visualizar media positiva dos alunos
+      Given o aluno "João Vasconcelos", com login "jvsn", possui média "8" em "Requisitos de Sistemas"
+      When eu solicito a página "Visualização"
+      Then a média do aluno "João Vasconcelos", com login "jvsn", em "Requisitos de Sistemas" aparece verde e com uma seta para cima
+ */
+/*
+Given(~'^o aluno "([^"]*)", com login "([^"]*)", possui média "([^"]*)" em "([^"]*)"$'){
+    String aluno, String login, double media, String conceito ->
+        to AddStudentsPage
+        at AddStudentsPage
+        AddStudentsPage.fillStudentDetails(aluno,login)
+        page.selectAddStudent()
+        to CreateCriterionPage
+        at CreateCriterionPage
+}
+*/
