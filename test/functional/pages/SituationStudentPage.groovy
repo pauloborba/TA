07e0fcc02ac9be32cbcdece95ddac36e7025b8c3
Lavinia Paganini <lfp2@cin.ufpe.br>
2016-11-30 22:46:34 -0300

    [#128919787] Atualizando a page

--- test/functional/pages/SituationStudentPage.groovy
+++ test/functional/pages/SituationStudentPage.groovy
@@ -8,8 +8,8 @@ class SituationStudentPage extends Page{
     static url = "/TA/situationStudents/index"
 
     static at =  {
-        //title ==~ /SituationStudents List/
-        title ==~ /Create property Listagem/
+        title ==~ /SituationStudents List/
+        //title ==~ /Create property Listagem/
     }
 
     boolean mediaVerde(aluno, conceito){
