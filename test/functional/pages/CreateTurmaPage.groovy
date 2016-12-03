1711193cab5fb7fbaf1fc9181d1f078a360677f9
JadsonLucena <jtl@cin.ufpe.br>
2016-12-03 00:05:10 -0300

    Reporting Classes, segunda iteração:  - A index.gsp foi modificada para as turmas com média baixa ficarem em vermelho  - Os testes foram melhorados para abrangerem melhor os casos críticos  - Novas funções foram criadas na class page para comportar melhor os novos recursos  - A class domain foi removida por não influenciar em uma aplicação específica de GUI
    Inclui os Stubs para que meu projeto funcione corretamente

---
+++ test/functional/pages/CreateTurmaPage.groovy
@@ -0,0 +1,33 @@
+package pages
+
+import geb.Page
+
+/**
+ * Created by dquei on 10/25/2016.
+ */
+class CreateTurmaPage extends Page{
+
+    static url = "/TA/turma/create"
+
+
+    static at = {
+        title ==~ /Create Turma/
+    }
+
+    def fillTurmaDetails(String id, String periodo){
+        $("form").classID = id
+        $("form").periodo = periodo
+    }
+
+    def selectCreateTurma(){
+        $("input", name: "create").click()
+    }
+
+    def boolean checkForErrors() {
+        return $("ul", class: "errors").isDisplayed()
+    }
+
+}
+
+
+
