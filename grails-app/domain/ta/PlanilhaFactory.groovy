640571e65a99247a52a6b8b7517b6581d9eb5bab
Isaac Douglas <isaacdouglas2015@gmail.com>
2017-06-07 21:12:35 -0300

    lib e planilhas

---
+++ grails-app/domain/ta/PlanilhaFactory.groovy
@@ -0,0 +1,26 @@
+package ta
+
+/**
+ * Created by Isaac Douglas on 07/06/17.
+ */
+public class PlanilhaFactory {
+
+    /**
+     * retorna uma planilha
+     * @param url url do arquivo da planilha
+     * @param tipoPlanilha string especificando o tipo da planilha
+     * @return uma planilha ou null se a planilha nao foi especificada corretamente
+     */
+    static Planilha getPlanilha(String url, String tipoPlanilha){
+
+        if (tipoPlanilha.equalsIgnoreCase("addaluno")){
+            //aqui retorna uma planilha com alunos
+        }
+
+        if (tipoPlanilha.equalsIgnoreCase("avaliacao")){
+            //aqui retorna uma plalinha com avaliacoes
+        }
+
+        return null
+    }
+}
