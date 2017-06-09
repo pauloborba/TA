5974fc090091563a048dd71dfb727c60a9dadc94
Wellington Felix <wfmf@cin.ufpe.br>
2017-06-09 09:06:35 -0300

    classes de planilha

---
+++ grails-app/domain/ta/PlanilhaAlunos.groovy
@@ -0,0 +1,7 @@
+package ta
+
+/**
+ * Created by wellington.felix on 6/9/17.
+ */
+class PlanilhaAlunos {
+}
---
+++ grails-app/domain/ta/PlanilhaFactory.groovy
@@ -0,0 +1,27 @@
+package ta
+
+/**
+ * Created by Isaac Douglas on 07/06/17.
+ */
+class PlanilhaFactory {
+
+    /**
+     * retorna uma planilha
+     * @param url url do arquivo da planilha
+     * @param tipoPlanilha string especificando o tipo da planilha
+     * @return uma planilha ou null se a planilha nao foi especificada corretamente
+     */
+    static Planilha getPlanilha(String url, String tipoPlanilha){
+
+        if (tipoPlanilha.equalsIgnoreCase("addaluno")){
+            //aqui retorna uma planilha com alunos
+        }
+
+        if (tipoPlanilha.equalsIgnoreCase("avaliacao")){
+            //aqui retorna uma plalinha com avaliacoes
+            return new PlanilhaAvaliacao(url)
+        }
+
+        return null
+    }
+}
